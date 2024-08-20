07b4110af22b26ff866d87f3647c26a45a0b60b6
itbebop <it.bebop@gmail.com>
2024-08-20 15:08:55 +0900

    feat: vo todo 생성

---
+++ lib/data/memory/vo/vo_todo.dart
@@ -0,0 +1,17 @@
+import 'package:fast_app_base/data/memory/vo/todo_status.dart';
+
+class Todo {
+  Todo({
+    required this.id,
+    required this.title,
+    required this.dueDate,
+    this.status = TodoStatus.incomplete,
+  }) : createdTime = DateTime.now();
+  // createdTime은 생성자가 호출되는 시점에 만들어지면 되므로
+  int id;
+  String title;
+  final DateTime createdTime;
+  DateTime? modifyTime;
+  DateTime dueDate;
+  TodoStatus status;
+}
